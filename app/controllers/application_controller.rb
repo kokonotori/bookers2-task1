8f3c9816b6201bc8b1a9851c0beb7c079b48ad57
EC2 Default User <ec2-user@ip-172-31-58-73.ec2.internal>
2021-09-03 14:20:53 +0000

    [modified]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+    flash[:notice]="successfully"
+    user_path(resource)
   end
 
 
@@ -12,5 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name,:password])
   end
 end
